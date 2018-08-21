69947cce4dfdc25d72309ebc4316cf0519c6a368
Тригубов Евгений Игоревич (ДИТ) <evgentrigub@yandex.ru>
2018-08-21 17:43:25 +0300

    Первый коммит

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinBotApi.Controllers
+{
+    //[Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        //ПРРОВЕРКА НА ВХОД ПО JWS-TOKEN
+
+        //[Authorize]
+        //[Route("getlogin")]
+        //public IActionResult GetLogin()
+        //{
+        //    return Ok($"Ваш логин: {User.Identity.Name}");
+        //}
+
+        //[Authorize(Roles = "admin")]
+        //[Route("getrole")]
+        //public IActionResult GetRole()
+        //{
+        //    return Ok("Ваша роль: администратор");
+        //}
+    }
+}

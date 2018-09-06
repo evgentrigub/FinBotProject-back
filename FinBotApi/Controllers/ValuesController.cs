0e3cc139ffebdf87a99a751101b401e3d1f2d21b
Тригубов Евгений Игоревич (ДИТ) <evgentrigub@yandex.ru>
2018-09-06 19:09:21 +0300

    фиксы

--- FinBotApi/Controllers/ValuesController.cs
+++ FinBotApi/Controllers/ValuesController.cs
@@ -5,9 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace FinBotApi.Controllers
 {
-    //[Route("api/[controller]")]
+    [Route("api/[controller]")]
     public class ValuesController : Controller
     {
         //ПРРОВЕРКА НА ВХОД ПО JWS-TOKEN
@@ -25,5 +26,37 @@ namespace FinBotApi.Controllers
         //{
         //    return Ok("Ваша роль: администратор");
         //}
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
     }
 }

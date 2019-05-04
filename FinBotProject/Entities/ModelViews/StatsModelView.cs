f4fd9a4a0f268dc1476b84b768877cfc9b10aec1
Unknown <ilyitatjana@yandex.ru>
2019-05-04 09:21:53 +0300

    Создана вью модель для отображения статистики

---
+++ FinBotProject/Entities/ModelViews/StatsModelView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApi.Entities.ModelViews
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("tag-name")]
+    public class StatsModel : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
